e87575eead418af35ff30137589e6fb3529e8dc7
Abhinav Verma <abhi.verma@tamu.edu>
2017-08-02 00:29:24 -0500

    Create Power of Three.py

---
+++ Python/Power of Three.py
@@ -0,0 +1,18 @@
+
+class Solution(object):
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        
+        if n < 1:
+          return False
+        
+        while n % 3 == 0:
+          n = n // 3
+        
+        return n == 1
+        
+a = Solution()
+print(a.isPowerOfThree(10))
